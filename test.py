131cdd337d62df3ebbc779a03c4a4bc81f781de5
irfan309 <itikpirfan@gmail.com>
2020-09-01 20:17:12 +0530

    identifier and reserved words of python

---
+++ test.py
@@ -0,0 +1,32 @@
+# identifiers 
+
+Adil_Name = 'Adil khan True'
+
+cash = 1000
+
+Cash = 20000
+print(type(Cash))
+
+print(Adil_Name)
+
+print(cash)
+
+print(Cash)
+
+# and = 100
+# print(and)
+
+# class = 20000
+# print(class)
+
+# def = 'irfan'
+# print(def)
+
+import keyword
+print(keyword.kwlist)
+
+def calc():
+    a = 10
+    b = 20000
+
+    print(a+b)
